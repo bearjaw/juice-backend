25d1e8860db9b85c073d3e42f2f4708657bb3dc8
Max Baumbach <bearjaw.dev@gmail.com>
2020-06-07 21:22:18 +0200

    Add boot test case

--- Tests/AppTests/PlaylistControllerTests.swift
+++ Tests/AppTests/PlaylistControllerTests.swift
@@ -7,10 +7,15 @@ class AppTests: XCTestCase {
     func testPlaylistController_init() throws {
         XCTAssertNotNil(PlaylistController())
     }
+
+    func testBoot_registers_routes() {
+        let controller = PlaylistController()
+        let router = EngineRouter.default()
+        XCTAssertNoThrow(try controller.boot(router: router))
     }
     
     static let allTests = [
         ("testPlaylistController_init", testPlaylistController_init),
+        ("testBoot_registers_routes", testBoot_registers_routes)
     ]
 }
-
