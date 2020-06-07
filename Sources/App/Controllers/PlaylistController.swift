d4c3279b55061cbb3dabefc30e12915aa5f8b366
Max Baumbach <bearjaw.dev@gmail.com>
2020-06-07 18:33:14 +0200

    Implement PlaylistController

--- Sources/App/Controllers/PlaylistController.swift
+++ Sources/App/Controllers/PlaylistController.swift
@@ -11,7 +11,16 @@ import FluentSQLite
 
 final class PlaylistController {
 
-    func add(_ req: Request) throws -> Future<UserToken> {
+    func list(_ req: Request) throws -> Future<[PlaylistListItemResponse]> {
+        let user = try req.requireAuthenticated(User.self)
+
+        return try Playlist.query(on: req)
+            .filter(\.userID == user.requireID())
+            .all()
+            .map { $0.map(PlaylistListItemResponse.create(from:)) }
+    }
+
+    func playlist(_ req: Request) throws -> Future<UserToken> {
         // get user auth'd by basic auth middleware
         let user = try req.requireAuthenticated(User.self)
 
@@ -22,7 +31,7 @@ final class PlaylistController {
         return token.save(on: req)
     }
 
-    func remove(_ req: Request) throws -> Future<UserToken> {
+    func all(_ req: Request) throws -> Future<UserToken> {
         // get user auth'd by basic auth middleware
         let user = try req.requireAuthenticated(User.self)
 
@@ -34,3 +43,33 @@ final class PlaylistController {
     }
     
 }
+
+struct PlaylistListItemRequest: Content {
+
+    let id: Int
+
+}
+
+struct PlaylistListItemResponse: Content {
+
+    let id: Int
+
+    let title: String
+
+    let description: String?
+
+    let itemCount: Int
+
+}
+
+extension PlaylistListItemResponse {
+
+    static func create(from playlist: Playlist) -> PlaylistListItemResponse {
+        PlaylistListItemResponse(id: playlist.id ?? 0,
+                                 title: playlist.title,
+                                 description: playlist.description,
+                                 itemCount: 0)
+    }
+
+}
+
