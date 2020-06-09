e41fe923bf7d69fe96cccdffc024b98f40cba1ba
Max Baumbach <bearjaw.dev@gmail.com>
2020-06-09 21:31:45 +0200

    Add basic endpoint

---
+++ Sources/App/API/AppleMusic/MusicEndpoints.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Max Baumbach on 09/06/2020.
+//
+
+import Foundation
+
+enum MusicEnpoint {
+
+    static let base = "https://api.music.apple.com"
+
+    static let version = "v1"
+
+}
