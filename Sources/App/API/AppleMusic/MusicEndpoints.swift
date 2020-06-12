e59ffbc299ec6272fa445131cdd17ef7245bf4e8
Max Baumbach <bearjaw.dev@gmail.com>
2020-06-12 23:04:10 +0200

    Add Apple Music endpoints

--- Sources/App/API/AppleMusic/MusicEndpoints.swift
+++ Sources/App/API/AppleMusic/MusicEndpoints.swift
@@ -7,10 +7,31 @@
 
 import Foundation
 
-enum MusicEnpoint {
+enum MusicEnpoint: String {
+
+    case activity
+    case albums
+    case artists
+    case catalog
+    case charts
+    case curators
+    case genres
+    case history
+    case me
+    case playlist
+    case ratings
+    case recommendations
+    case search
+    case songs
+    case videos
 
     static let base = "https://api.music.apple.com"
 
     static let version = "v1"
+
+    var endpoint: String {
+        let baseURL = "\(MusicEnpoint.base)/\(MusicEnpoint.version)/"
+        return "\(baseURL)\(MusicEnpoint.catalog.rawValue)/us/\(self.rawValue)"
+    }
 
 }
