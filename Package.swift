572c800e89ffc57105f0dbe55762847164bbe6d9
Max Baumbach <bearjaw.dev@gmail.com>
2020-06-12 22:00:47 +0200

    Update MusicCore

--- Package.swift
+++ Package.swift
@@ -1,8 +1,9 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
     name: "juice",
+    platforms: [.macOS(.v10_12)],
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
