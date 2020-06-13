675946ee9eed3a0e23589e147c8ffd3e3683954e
Max Baumbach <bearjaw.dev@gmail.com>
2020-06-13 18:17:28 +0200

    Add isNonEmpty Collection Extension

---
+++ Sources/App/Extensions/Foundation+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  
+//
+//  Created by Max Baumbach on 13/06/2020.
+//
+
+import Foundation
+
+extension Collection {
+
+    var isNonEmpty: Bool {
+        !isEmpty
+    }
+
+}
