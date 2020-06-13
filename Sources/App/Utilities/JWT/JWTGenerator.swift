ab8c048fa81af37c8eaf2c0bacad215322dd6bea
Max Baumbach <bearjaw.dev@gmail.com>
2020-06-13 18:23:17 +0200

    Implement JWT Generator Wrapper

---
+++ Sources/App/Utilities/JWT/JWTGenerator.swift
@@ -0,0 +1,90 @@
+//
+//  JWTGenerator.swift
+//  
+//
+//  Created by Max Baumbach on 09/06/2020.
+//
+
+import JWT
+
+struct JWTGenerator {
+
+    enum Validity {
+        case day
+        case custom(TimeInterval)
+
+        var value: TimeInterval {
+            switch self {
+            case .day:
+                return 60*60*24
+            case let .custom(value):
+                return value
+            }
+        }
+    }
+
+    enum Algorithm {
+        case es256(data: Data)
+
+        var string: String {
+            switch self {
+            case .es256:
+                return "ES256"
+            }
+        }
+
+        var signer: JWTSigner {
+            switch self {
+            case let .es256(data):
+                return .init(algorithm: ES256(key: data))
+            }
+        }
+    }
+
+    enum JWTGeneratorError: Error {
+        case signing
+    }
+
+    /// Genrate a JWT Token
+    ///
+    /// - Parameters:
+    ///   - kId: The key ID
+    ///   - iss: The token issuer id
+    ///   - validity: The period the token should stay valid
+    ///   - algorithm: The algorithm used
+    /// - Throws: Throws a `JWTGeneratorError` if signing fails
+    /// - Returns: The Token as a String
+    static func generateToken(kId: String,
+                              iss: String,
+                              validity: Validity = .day,
+                              algorithm: Algorithm) throws -> String {
+
+        let header = JWTHeader(alg: algorithm.string,
+                               kid: kId)
+
+        let iat = Date()
+        let exp = iat.addingTimeInterval(validity.value).timeIntervalSince1970.rounded()
+
+        let payload = AppleMusicToken(iss: iss,
+                                      iat: iat.timeIntervalSince1970.rounded(),
+                                      exp: exp)
+
+        return ""
+    }
+    
+}
+
+extension JWTGenerator {
+
+    static func createAppleMusicToken() throws -> String {
+        let pKey = """
+        Load your private key
+        """
+        let data = pKey.data(using: .utf8)!
+        let algorithm = JWTGenerator.Algorithm.es256(data: data)
+        return try JWTGenerator.generateToken(kId: "",
+                                              iss: "",
+                                              algorithm: algorithm)
+
+    }
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -14,6 +14,7 @@ public func routes(_ router: Router) throws {
     let playlistController = PlaylistController()
     try playlistController.boot(router: router)
 
-    let songsController = SongsController()
-    try songsController.boot(router: router)
+    let songsController = AppleMusicSearchController()
+    let token = try JWTGenerator.createAppleMusicToken()
+    try songsController.boot(router: router, with: token)
 }
