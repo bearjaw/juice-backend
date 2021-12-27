4d58c45aced16ff5a25637a46d8754e9d355a83b
Max Baumbach <max.baumbach@snappcar.nl>
2021-12-27 19:42:58 +0100

    Update Package versions

--- Package.swift
+++ Package.swift
@@ -12,8 +12,10 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.0.0"),
         .package(url: "https://github.com/bearjaw/MusicCore.git", from: "1.1.0"),
-        .package(url: "https://github.com/vapor/jwt.git", from: "4.0.0-rc"),
-        .package(url: "https://github.com/johnsundell/Plot.git", from: "0.7.0")
+        .package(url: "https://github.com/vapor/jwt.git", from: "4.0.0"),
+        .package(url: "https://github.com/johnsundell/Plot.git", from: "0.7.0"),
+        .package(url: "https://github.com/vapor/leaf", from: "4.0.0"),
+            .package(url: "https://github.com/vapor/leaf-kit", from: "1.0.0")
     ],
     targets: [
         .target(
@@ -24,6 +26,7 @@ let package = Package(
                 .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
                 .product(name: "MusicCore", package: "MusicCore"),
                 .product(name: "Vapor", package: "vapor"),
+                .product(name: "Leaf", package: "Leaf"),
                 "Plot"
             ],
             swiftSettings: [
